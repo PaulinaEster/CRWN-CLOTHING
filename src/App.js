c36f928bbf58459c4ef8f13bf485da2aa15a65ae
Paulina Ester Egvarth Rehbein <paulinarehbein@Paulinas-MacBook-Pro.local>
2022-01-25 10:03:30 -0300

    criado header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { Route, Routes } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <p>HATS PAGE</p>
@@ -9,12 +10,15 @@ const HatsPage = () => (
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<Homepage/>} />
-      <Route path='shop/hats' element={<HatsPage/>} />
-      <Route exact path='shop' element={<ShopPage/>} />
-      {/* <Route path='' element={< />} /> */}
-    </Routes>
+    <div>
+      <Header/>
+      <Routes>
+        <Route exact path='/' element={<Homepage/>} />
+        <Route path='shop/hats' element={<HatsPage/>} />
+        <Route exact path='shop' element={<ShopPage/>} />
+        {/* <Route path='' element={< />} /> */}
+      </Routes>
+    </div>
   );
 }
 
